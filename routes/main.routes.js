69208660397c39093cd7b9ca1b31a032381d401f
ojavier <ojvilleda6@gmail.com>
2025-03-21 12:09:21 -0600

    Cración de main.routes

---
+++ routes/main.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+const mainController = require('../controllers/main.controller');
+
+
+
+
+router.get('/', mainController.getSidebar);
+
+
+
+
+module.exports = router;
