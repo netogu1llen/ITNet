72e95dbb3fed97f60fb9ffa0dd3a0420a6dfa71d
Diego Ponce de Leon <a01664407@tec.mx>
2025-03-27 00:29:10 -0600

    Solucion de conflictos

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -8,13 +8,10 @@ const mainController = require('../controllers/main.controller');
 const s3Routes = require('./s3services.routes');
 
 // Rutas principales
-<<<<<<< HEAD
 router.get('/', mainController.getClinicaV2);
-=======
 router.get('/', mainController.getHome);
 router.get('/', mainController.getPacientes);
 router.get('/', mainController.getTrabajadores);
->>>>>>> 98e914f9713ca478527d1c349892acfb39431434
 
 // Usar las rutas de S3 con un prefijo "/api"
 router.use('/api', s3Routes);
