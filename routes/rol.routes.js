48ce3eb15d1461e8be3783d99549334e8adce60e
masterCrafter <mastercrafter@MacBook-Pro-de-masterCrafter.local>
2025-04-12 15:16:36 -0600

    historia terminada :D

---
+++ routes/rol.routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const rolController = require('../controllers/rol.controller');
+
+// Ruta para obtener los datos de los usuarios (raíz del módulo)
+router.get('/', rolController.get_roles);
+
+module.exports = router;
