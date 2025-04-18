baa0bf05bdd315f1e07db7619a4130c6d355a3a3
masterCrafter <mastercrafter@MacBook-Pro-de-masterCrafter.local>
2025-04-18 00:06:30 -0600

    correciones rutas

--- routes/educacion.routes.js
+++ routes/educacion.routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const canVerExpediente = requiere('../util/can-verExpediente');
+const canVerExpediente = require('../util/can-verExpediente');
 const canRegistrarBoleta = require('../util/can-registrarBoleta');
 const canEditarBoleta = require('../util/can-editarBoleta');
 const canEliminarBoleta = require('../util/can-eliminarBoleta');
-const canConsultarBoleta = require('../util/can-consultarBoleta');
+const canConsultarBoletas = require('../util/can-consultarBoletas');
 const canRegistrarMateria = require('../util/can-registrarMateria');
 const canEditarMateria = require('../util/can-editarMateria');
-const canEliminarBoleta = require('../util/can-eliminarBoleta');
+const canEliminarMateria = require('../util/can-eliminarMateria');
 const canEditarCalificacion = require('../util/can-editarCalificacion');
 
 const controller = require('../controllers/educacion.controller');
