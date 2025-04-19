0ce50827b61198002fd4b887ce87c6064a1f4fd7
ojavier <ojvilleda6@gmail.com>
2025-04-19 12:39:20 -0600

    Eliminar main.routes

--- routes/main.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Importar controladores
-const mainController = require('../controllers/main.controller');
-
-
-module.exports = router;
